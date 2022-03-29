eb44604587e6b9c7a5a2dce6d662312513c1e8b3
otrachea <francesca.ho459@gmail.com>
2022-03-29 20:30:47 +0000

    Populate tweets from js file instead of using hardcoded tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-const createTweetElement = function() {
+$(document).ready(function() {
 
-}
+  const createTweetElement = function (tweetData) {
+    return $(`
+      <header>
+        <div>
+          <img src="${tweetData.user.avatars}">
+          <div>${tweetData.user.name}</div>
+        </div>
+        <div>${tweetData.user.handle}</div>
+      </header>
+      <div>
+        ${tweetData.content.text}
+      </div>
+      <hr>
+      <footer>
+        <span>${tweetData.created_at}</span>
+        <div>
+          <i class="fa-solid fa-flag fa-xs"></i>
+          <i class="fa-solid fa-retweet fa-sm"></i>
+          <i class="fa-solid fa-heart fa-xs"></i>
+        </div>
+      </footer>`);
+  }
 
-const tweetData =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  }
 
-const $tweet = createTweetElement(tweetData)
-console.log($tweet);
-$('.tweets-container').append($tweet);
+  const $tweet = createTweetElement(tweetData)
+  $('.tweets-container').append($tweet);
+
+});
