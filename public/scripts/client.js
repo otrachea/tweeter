2cd92947c5de8e876718afac83dd5dc6a1eae8a3
otrachea <francesca.ho459@gmail.com>
2022-03-29 21:17:39 +0000

    Make days ago counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
       </div>
       <hr>
       <footer>
-        <span>${tweetData.created_at}</span>
+        <span>${Math.floor(((new Date()).getTime() - tweetData.created_at) / (1000 * 3600 * 24))} days ago</span>
         <div>
           <i class="fa-solid fa-flag fa-xs"></i>
           <i class="fa-solid fa-retweet fa-sm"></i>
@@ -38,7 +38,7 @@ $(document).ready(function() {
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1648402475045
   }
 
   const $tweet = createTweetElement(tweetData)
