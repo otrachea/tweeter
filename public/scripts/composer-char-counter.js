7caa216e1448866e197c3fe7ea6a1f040fadf7d7
otrachea <francesca.ho459@gmail.com>
2022-03-29 15:34:27 +0000

    Get counter using DOM traversal instead of selecting the counter by class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,9 @@ $(document).ready(() => {
   $(".counter").text(140 - $("#tweet-text").val().length);
 
   $("#tweet-text").on("input", function() {
-    $(".counter").text(140 - $(this).val().length);
+    $(this)
+    .siblings("div")
+    .children("output")
+    .text(140 - $(this).val().length);
   })
 });
