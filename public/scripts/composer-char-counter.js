22c718859372b3c27df4ce8e0e60e1ec1a0a8e2a
otrachea <francesca.ho459@gmail.com>
2022-03-29 16:08:47 +0000

    On refresh clear new tweet in textarea and refactor selector for #tweet-text to variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,24 @@
-$(document).ready(() => {
-  $(".counter").text(140 - $("#tweet-text").val().length);
+const clear = function(selector) {
+  $(selector).val("");
+}
 
-  $("#tweet-text").on("input", function() {
-    $(this)
-    .siblings("div")
-    .children("output")
-    .text(140 - $(this).val().length);
+$(document).ready(() => {
+
+  clear("#tweet-text")
+
+  $("#tweet-text").on("input", function () {
+    const counter = $(this)
+      .siblings("div")
+      .children("output");
+
+    // sets counter text
+    counter.text(140 - $(this).val().length);
+
+    // if over 140 characters, counter is now red
+    if ($(this).val().length > 140 ) {
+      counter.css("color", "#ff0000");
+    } else {
+      counter.css("color", "");
+    }
   })
 });
