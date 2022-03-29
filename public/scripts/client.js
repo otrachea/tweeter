ec9709f6baecfbc509e0dc0847a47238b0405f0f
otrachea <francesca.ho459@gmail.com>
2022-03-29 22:19:10 +0000

    Remove hardcoded driver code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -44,32 +44,6 @@ $(document).ready(function () {
     });
   }
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1648402475045
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1648488875045
-    }
-  ];
-
-  // renderTweets(tweetData);
   loadTweets();
 
   $("form").on("submit", function (event) {
