c24e84dc374c192537cd697b755e8e705fddac03
otrachea <francesca.ho459@gmail.com>
2022-03-29 21:26:24 +0000

    Each tweet renders onto page using javascript

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   const createTweetElement = function (tweetData) {
     return $(`
-      <header>
+      <article>
+        <header>
+          <div>
+            <img src="${tweetData.user.avatars}">
+            <div>${tweetData.user.name}</div>
+          </div>
+          <div>${tweetData.user.handle}</div>
+        </header>
         <div>
-          <img src="${tweetData.user.avatars}">
-          <div>${tweetData.user.name}</div>
+          ${tweetData.content.text}
         </div>
-        <div>${tweetData.user.handle}</div>
-      </header>
-      <div>
-        ${tweetData.content.text}
-      </div>
-      <hr>
-      <footer>
-        <span>${Math.floor(((new Date()).getTime() - tweetData.created_at) / (1000 * 3600 * 24))} days ago</span>
-        <div>
-          <i class="fa-solid fa-flag fa-xs"></i>
-          <i class="fa-solid fa-retweet fa-sm"></i>
-          <i class="fa-solid fa-heart fa-xs"></i>
-        </div>
-      </footer>`);
+        <hr>
+        <footer>
+          <span>${Math.floor(((new Date()).getTime() - tweetData.created_at) / (1000 * 3600 * 24))} days ago</span>
+          <div>
+            <i class="fa-solid fa-flag fa-xs"></i>
+            <i class="fa-solid fa-retweet fa-sm"></i>
+            <i class="fa-solid fa-heart fa-xs"></i>
+          </div>
+        </footer>
+      </article>`
+    );
   }
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1648402475045
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      $('.tweets-container').append(createTweetElement(tweet));
+    }
   }
 
-  const $tweet = createTweetElement(tweetData)
-  $('.tweets-container').append($tweet);
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1648402475045
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1648488875045
+    }
+  ];
+
+  renderTweets(tweetData);
 
 });
