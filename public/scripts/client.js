73d584e63ba0608a9eb3b144161d2ca6336d4732
otrachea <francesca.ho459@gmail.com>
2022-04-01 02:27:59 +0000

    Navbar disappears in mobile mode and navbar settings fade out when scrolling down in mobile mode

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,12 +6,14 @@
 
 $(() => {
 
+  // Transforms any scripting attack to normal text
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
+  // Creates tweet element from tweetData
   const createTweetElement = function (tweetData) {
     return $(`
       <article>
@@ -36,92 +38,106 @@ $(() => {
         </footer>
       </article>`
     );
-  }
+  };
 
+  // Adds array of tweets to tweets-container with newest tweet at top
   const renderTweets = function (tweets) {
     for (const tweet of tweets) {
       $('.tweets-container').prepend(createTweetElement(tweet));
     }
-  }
+  };
 
+  // Uses ajax get request to get all tweets from server then renders them
   const loadTweets = function () {
     $.get("http://localhost:8080/tweets")
       .then(data => renderTweets(data));
-  }
+  };
 
   loadTweets();
 
+  // When "Tweet" button is clicked
   $("form").on("submit", function (event) {
+    // Prevents page from refreshing when submit button clicked
     event.preventDefault();
+
     let text = $(this).children("textarea");
 
+    // Error for empty tweet
     if (!text.val()) {
       $(".error-container span").text("Tweet cannot be empty");
       $(".error-container").slideDown();
       return;
     }
 
+    // Error for tweet over 140 chars
     if (text.val().length > 140) {
       $(".error-container span").text("Tweet must be 140 characters or less");
       $(".error-container").slideDown();
       return;
     }
 
+    // If tweet is valid, slides up error container if it was displayed before
     if ($(".error-container").css("display") !== "none") {
       $(".error-container").slideUp();
     }
 
+    // Sends tweet to server using ajax post request
     $.post("/tweets/", $(this).serialize(), () => {
+      // Clears textarea
       $(this).children("textarea").val("");
+
+      // After tweet saved to server, get all tweets from server again
       $.get("http://localhost:8080/tweets", function (data) {
-        let newTweet = createTweetElement(data.pop())
+        // Gets latest tweet from server, prepends to container and slides down
+        let newTweet = createTweetElement(data.pop());
         newTweet.css("display", "none");
         $('.tweets-container').prepend(newTweet);
-        newTweet.slideDown(); 
+        newTweet.slideDown();
       });
     });
+  });
 
-  })
-
-  const showNewTweet = function () {
+  // When compose tweet button is clicked, slides new tweet container up or down
+  $("nav > div > button").on("click", function () {
     let section = $(".new-tweet");
     if (section.css("display") === "none") {
       section.slideDown();
+      // Sets focus to textarea after sliding down
       $("#tweet-text").focus();
     } else {
       section.slideUp();
     }
-  }
+  });
 
-  $("nav > div > button").on("click", showNewTweet);
-
+  // When clicking scroll up button, smooth scrolls to top of window
   $("main > button").on("click", function () {
-    console.log("a");
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  })
+  });
 
   const fadeIn = function (element) {
     element.addClass("fade-in");
     element.removeClass("fade-out");
-  }
+  };
 
   const fadeOut = function (element) {
     element.addClass("fade-out");
     element.removeClass("fade-in");
-  }
+  };
+
 
   $(document).on("scroll", function () {
+    let navCheck = (window.innerWidth >= 1024) ? "nav > div" : "nav";
     if (window.scrollY >= 120) {
       fadeIn($("main > button"));
-      fadeOut($("nav > div"));
+      fadeOut($(navCheck));
     } else {
       fadeOut($("main > button"));
-      fadeIn($("nav > div"));
+      fadeIn($(navCheck));
     }
-  })
+  });
 
   $("#tweet-text").on("input", function () {
     this.style.height = "auto";
     this.style.height = (this.scrollHeight) + 'px';
-  })
+  });
 });
